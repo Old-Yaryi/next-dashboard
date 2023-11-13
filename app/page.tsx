319c402b92c9b3dbbe6af3f9cf34d5129b3d5e03
Yaryi <kinomir-v@yandex.ru>
2023-11-13 23:15:19 +0300

    second-3

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import styles from '@/app/ui/home.module.css';
 import Link from 'next/link';
-// import { lusitana } from './ui/fonts';
+// import { lusitana }  1 from './ui/fonts';
 import Image from 'next/image';
 
 
